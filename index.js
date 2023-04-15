bc7b40ee704882547014f82c3849ea6645c35a4d
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-04-15 23:49:56 +0100

    add an emoji to the array

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"];
 const emojiEl = document.getElementById("emoji-container");
 const pushBtn = document.getElementById("push-btn");
-const inputEl = document.getElementById("emoji-input");
 
-for (let i = 0; i < myEmojis.length; i++) {
-  const emojiSpan = document.createElement("span");
-  emojiSpan.textContent = myEmojis[i];
-  emojiEl.append(emojiSpan);
+function renderEmojis() {
+  for (let i = 0; i < myEmojis.length; i++) {
+    const emojiSpan = document.createElement("span");
+    emojiSpan.textContent = myEmojis[i];
+    emojiEl.append(emojiSpan);
+  }
 }
 
-pushBtn.addEventListener('click', function(){
-    console.log(inputEl.value);
-})
+renderEmojis();
+
+pushBtn.addEventListener("click", function () {
+  const inputEl = document.getElementById("emoji-input");
+  const emoji = inputEl.value;
+  if (emoji) {
+    myEmojis.push(emoji);
+    inputEl.value = "";
+    emojiEl.innerHTML = "";
+    renderEmojis();
+  }
+});
