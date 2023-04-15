e822cfec6929f7b15f05be0f3e624ab1f36fd2a8
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-04-15 20:52:28 +0100

    Get the input field and logged it out

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"];
+const emojiEl = document.getElementById("emoji-container");
+const pushBtn = document.getElementById("push-btn");
+const inputEl = document.getElementById("emoji-input");
 
 for (let i = 0; i < myEmojis.length; i++) {
-  console.log(myEmojis[i]);
+  const emojiSpan = document.createElement("span");
+  emojiSpan.textContent = myEmojis[i];
+  emojiEl.append(emojiSpan);
 }
+
+pushBtn.addEventListener('click', function(){
+    console.log(inputEl.value);
+})
